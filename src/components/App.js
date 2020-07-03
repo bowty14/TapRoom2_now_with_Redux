0f6e34c81f4483a6fff5e39ddd2c3aeb99d253a2
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-02 21:02:24 -0700

    add css styling to body and header.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header'
+// import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className='header'>
+        <Header />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
