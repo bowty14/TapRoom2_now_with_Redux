351dfc08e364790fe860f7331702debc54549807
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-03 09:01:21 -0700

    add a handle click function for state.

--- src/components/tapRoom.js
+++ src/components/tapRoom.js
@@ -1,1 +1,34 @@
 import React from 'react';
+import NewKombuchaForm from './NewKombuchaForm';
+import KombuchaList from './KombuchaList';
+import KombuchaDetail from './KombuchaDetail';
+import EditKombuchaForm from './EditKombuchaForm';
+
+class TapRoom extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      masterKombuchaList: [],
+      selectedKombucha: null,
+      editing: false
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedKombucha != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKombucha: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
+  
+}
