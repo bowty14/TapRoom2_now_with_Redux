ee6c851052e8ac531f653eccf90d48ba7ce08a3f
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-03 09:49:16 -0700

    add functions for buying a pint and restocking kegs.

--- src/components/tapRoom.js
+++ src/components/tapRoom.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NewKombuchaForm from './NewKombuchaForm';
-import KombuchaList from './KombuchaList';
+import KombuchaList from './kombuchaList';
 import KombuchaDetail from './KombuchaDetail';
 import EditKombuchaForm from './EditKombuchaForm';
 
@@ -38,6 +38,57 @@ class TapRoom extends React.Component {
     });
   }
   
-  
+  handleChangingSelectedKombucha = (id) => {
+    const selectedKombucha = this.state.masterKombuchaList.filter(kombucha => kombucha.id === id)[0];
+    this.state({ selectedKombucha: selectedKombucha });
+  }
+
+  handleDeletingKombucha = (id) => {
+    const newMasterKombuchaList = this.state.masterKombuchaList.filter(kombucha => kombucha.id !== id);
+    this.setState({
+      masterKombuchaList: newMasterKombuchaList,
+      selectedKombucha: null
+    });
+  }
+
+  handleEditClick = () => {
+    this.setState({ editing: true });
+  }
+
+  handleEditingKombuchaInList = (kombuchaToEdit) => {
+    const editedMasterKombuchaList = this.state.masterKombuchaList.filter(kombucha => kombucha.id !== this.state.selectedKombucha.id).concat(kombuchaToEdit);
+    this.setState({
+      masterKombuchaList: editedMasterKombuchaList,
+      editing: false,
+      selectedKombucha: null
+    });
+  }
+
+  handleBuyingAPint = (id) => {
+    const purchasedPint = this.state.masterKombuchaList.filter(kombucha => kombucha.id === id)[0];
+    purchasedPint.quantity -= 1;
+    const editedMasterKombuchaList = this.state.masterKombuchaList.filter(kombucha => kombucha.id !== this.state.selectedKombucha.id).concat(purchasedPint);
+    this.setState({
+      masterKombuchaList: editedMasterKombuchaList
+    });
+  }
+
+  handleTappingAKeg = (id) => {
+    const restockKombucha = this.state.masterKombuchaList.filter(kombucha => kombucha.id === id)[0];
+    purchasedPint.quantity -= 1;
+    const editedMasterKombuchaList = this.state.masterKombuchaList.filter(kombucha => kombucha.id !== this.state.selectedKombucha.id).concat(restockKombucha);
+    this.setState({
+      masterKombuchaList: editedMasterKombuchaList
+    });
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.)
+  }
 
 }
+
+export default TapRoom;
