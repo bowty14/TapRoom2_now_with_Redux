3739ca4a4e249580d9aa2fa972c28548aca64a0c
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-06 16:28:23 -0700

    fix an issue where when editting if fields are blank it updates the object with blank fields.

--- src/components/EditKombuchaForm.js
+++ src/components/EditKombuchaForm.js
@@ -7,6 +7,21 @@ function EditKombuchaForm(props) {
   const { kombucha } = props;
   function handleEditKombuchaFormSubmission(event) {
     event.preventDefault();
+    if (event.target.name.value === "") {
+      event.target.name.value = kombucha.name;
+    }
+    if (event.target.brand.value === "") {
+      event.target.brand.value = kombucha.brand;
+    } 
+    if (event.target.price.value === "") {
+      event.target.price.value = kombucha.price
+    }
+    if (event.target.flavor.value === "") {
+      event.target.flavor.value = kombucha.flavor
+    }
+    if (event.target.quantity.value === "") {
+      event.target.quantity.value = kombucha.quantity
+    }
     props.onEditKombucha({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.flavor.value, quantity: event.target.quantity.value, id: kombucha.id });
   }
 
