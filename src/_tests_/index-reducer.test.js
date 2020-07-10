fcfe0111aeffd018a70857dc34896a37cdc2f95c
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-10 10:39:10 -0700

    add tests that will check that root reducer reflects the changes made to state when we pass an action.both tests pass.

--- src/_tests_/index-reducer.test.js
+++ src/_tests_/index-reducer.test.js
@@ -12,12 +12,34 @@ describe('rootReducer', () => {
       formVisibleOnPage: false
     });
   });
-      test('Check that initial state of kombuchListReducer matches root reducer', () => {
-        expect(store.getState().masterKombuchaList).toEqual(kombuchaListReducer(undefined, { type: null }));
-      })
+  test('Check that initial state of kombuchListReducer matches root reducer', () => {
+    expect(store.getState().masterKombuchaList).toEqual(kombuchaListReducer(undefined, { type: null }));
+  });
   
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
-      })
+  });
+
+  test('Check that initial state of kombuchaListReducer matches root reducer', () => {
+    const action = {
+      type: 'ADD_KOMBUCHA',
+      name: 'Tropic Thunder',
+      brand: 'Kombucha market',
+      price: 6,
+      flavor: 'Pineapple',
+      quantity: 124,
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKombuchaList).toEqual(kombuchaListReducer(undefined, action));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
 
 });
