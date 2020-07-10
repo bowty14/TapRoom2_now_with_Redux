b891597bddab71252cb4842675dbf992d09b4758
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-07-10 10:31:56 -0700

    add 2 test checking that kombuchalistreducer and formvisiblereducer have the same default state as the root reducer. they both pass.

--- src/_tests_/index-reducer.test.js
+++ src/_tests_/index-reducer.test.js
@@ -1,4 +1,9 @@
-import rootReducer from '../../reducers/index';
+import rootReducer from '../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../reducers/form-visible-reducer';
+import kombuchaListReducer from '../reducers/kombucha-List-Reducer';
+
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
   test('Should return default state if no action type is recognized', () => {
@@ -7,5 +12,12 @@ describe('rootReducer', () => {
       formVisibleOnPage: false
     });
   });
+      test('Check that initial state of kombuchListReducer matches root reducer', () => {
+        expect(store.getState().masterKombuchaList).toEqual(kombuchaListReducer(undefined, { type: null }));
+      })
   
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+      })
+
 });
